9a6858295fd11c9800edba599c321e3a9446be6c
admin <arunkumar.gurram444@gmail.com>
2016-09-27 08:36:36 +0530

    Intial check

---
+++ gitproject/src/test/java/com/sampleproject/git/gitproject/AppTest.java
@@ -0,0 +1,20 @@
+package com.sampleproject.git.gitproject;
+
+import org.testng.annotations.*;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+	@Test (timeOut=240000)
+    
+    public void add() throws Exception {
+
+		int a = 10;
+		int b= 20;
+		System.out.println(a+b);
+	}
+
+}
